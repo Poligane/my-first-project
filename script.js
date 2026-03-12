e82dbdff39b0efc7c789a1714b916e1acb21f612
Poligane <166440278+Poligane@users.noreply.github.com>
2026-03-12 12:21:10 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let contactform = document.getElementById('log')
-
-let sendemail = (e) => {
-    e.preventDefault()
-
-    emailjs.sendForm('service_n7072m1', 'template_pe8knqa', '#log', 'tF8nZKSPztLNsxiDD')
-}
-
-contactform.addEventListener('submit', sendemail);
